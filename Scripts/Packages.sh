f118d1e914d85771797f24c6da83299cb0b93ebc
yjiawqgj <71002956+yjiawqgj@users.noreply.github.com>
2025-08-27 11:43:12 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -46,6 +46,9 @@ UPDATE_PACKAGE() {
 
 UPDATE_PACKAGE "taskplan" "sirpdboy/luci-app-taskplan" "master"
 UPDATE_PACKAGE "luci-app-adguardhome" "kenzok8/openwrt-packages" "master" "luci-app-adguardhome"
+# 测试新软件包
+UPDATE_PACKAGE "luci-app-natfrp" "natfrp/luci-app-natfrp" "master" "luci-app-natfrp"
+
 # UPDATE_PACKAGE "包名" "项目地址" "项目分支" "pkg/name，可选，pkg为从大杂烩中单独提取包名插件；name为重命名为包名"
 UPDATE_PACKAGE "argon" "sbwml/luci-theme-argon" "openwrt-24.10"
 UPDATE_PACKAGE "kucat" "sirpdboy/luci-theme-kucat" "js"
